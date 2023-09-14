0feeed2928a98cf9a96be19d5f3a62e970a3cf5b
JaseelaAlimahamed <jessibrototype@gmail,com>
2023-09-14 16:07:14 +0530

    verifytoken commit

--- router/router.js
+++ router/router.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const userController=require('../controllers/userController')
 const contactsController=require('../controllers/contactsController')
-
+const verifyToken= require('../middleware/verifyToken')
 //user signIN/signUp
 
 router.post('/signup',userController.userSignup) 
 router.post('/signin',userController.userSignin)
 
 //contacts
-router.post('/addContact',contactsController.addContact) 
-router.get('/getContacts',contactsController.getContacts) 
-router.put('/editContact',contactsController.updateContact)
-router.delete('/deleteContact',contactsController.deleteContact)
+router.post('/addContact',verifyToken,contactsController.addContact) 
+router.get('/getContacts',verifyToken,contactsController.getContacts) 
+router.put('/editContact',verifyToken,contactsController.updateContact)
+router.delete('/deleteContact',verifyToken,contactsController.deleteContact)
 
 module.exports = router;
